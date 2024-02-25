6a0fc2ac1d86b8da78d872aed48b82c42c59a5ac
A-Novychenko <A.Novychenko@outlook.com>
2024-02-25 22:25:02 +0200

    add login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,4 +6,8 @@ router.post("/login", (req, res) => {
     res.status(200).json({message: "success"})
 })
 
+router.post("/logout", (req, res) => { 
+    res.status(204).end()
+})
+
 module.exports = router
