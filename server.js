1c8c8545da84620db6c5db14700e038dbe0e0409
A-Novychenko <A.Novychenko@outlook.com>
2024-02-25 02:12:03 +0200

    add logger

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express")
-
+const logger = require("morgan")
+const cors = require('cors')
 require("dotenv").config()
 
-const { PORT} = process.env
+const { PORT } = process.env
+const authRouter = require("./routes/api/auth")
 
 const app = express()
 
-app.get("/", (req, res) => { 
-    res.send("Admin connection successful")
+const formatsLogger = app.get("env") === "development" ? "dev" : "short"
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/auth", authRouter)
+
+app.use((_, res) => { 
+    res.status(404).json(
+        { message: "Not Found" }
+    )
 })
 
 app.listen(PORT, () => { 
