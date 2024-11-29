5fa7ecf285fe8f5405338a6a88153a4405a9a83c
A-Novychenko <A.Novychenko@outlook.com>
2024-11-29 15:17:05 +0200

    fix

--- controllers/catalog/getAllCategories.js
+++ controllers/catalog/getAllCategories.js
@@ -17,3 +17,46 @@ const getAllCategories = async (req, res) => {
 };
 
 module.exports = getAllCategories;
+
+// const ASGCategory = require('../../models/asg/categories');
+
+// const getAllCategories = async (req, res) => {
+//   const { id } = req.query;
+
+//   try {
+//     const categories = await ASGCategory.aggregate([
+//       { $match: { parent_id: parseInt(id, 10) } }, // Перетворюємо id на число
+//       {
+//         $lookup: {
+//           from: 'asgimages', // Колекція зображень
+//           localField: 'id', // Поле категорії
+//           foreignField: 'product_id', // Поле у зображеннях
+//           as: 'images', // Додаємо поле зображень
+//         },
+//       },
+//       {
+//         $addFields: {
+//           img: {
+//             $arrayElemAt: ['$images.images', 0], // Додаємо перше зображення як рядок
+//           },
+//         },
+//       },
+//       { $unset: 'images' }, // Видаляємо зайве поле `images`
+//     ]);
+
+//     res.json({
+//       status: 'OK',
+//       code: 200,
+//       categories,
+//     });
+//   } catch (error) {
+//     console.error('Error fetching categories:', error);
+//     res.status(500).json({
+//       status: 'error',
+//       code: 500,
+//       message: 'Failed to fetch categories',
+//     });
+//   }
+// };
+
+// module.exports = getAllCategories;
