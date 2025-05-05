93ac8c7c4e9ee48c30e19b40fde26be10b77fd0d
A-Novychenko <A.Novychenko@outlook.com>
2025-05-05 17:48:00 +0300

    fix asg auth

--- helpers/serviceASG.js
+++ helpers/serviceASG.js
@@ -2,8 +2,13 @@ const axios = require('axios');
 
 // const getBearer = async () => {};
 
+const X_ASG_HEADER = process.env.X_ASG_HEADER;
+
 const serviceASG = axios.create({
   baseURL: 'https://online.asg.ua/api',
+  headers: {
+    'X-Asg-Header': X_ASG_HEADER,
+  },
 });
 
 // serviceASG.defaults.headers.common.Authorization = `Bearer ${data.access_token}`;
